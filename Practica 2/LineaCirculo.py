ad56f6181e13dbd90b05a9ad48b7628db3319fba
MbTapia <beatriztapiamath@gmail.com>
2025-02-23 20:24:51 -0400

    Update LineaCirculo.py

--- Practica 2/LineaCirculo.py
+++ Practica 2/LineaCirculo.py
@@ -1,1 +1,43 @@
+import math
 
+class Punto:
+    def _init_(self, x: float, y: float):
+        self.x = x
+        self.y = y
+
+    def _str_(self):
+        return f"Punto({self.x}, {self.y})"
+
+class Linea:
+    def _init_(self, p1: Punto, p2: Punto):
+        self.p1 = p1
+        self.p2 = p2
+
+    def dibujaLinea(self):
+        return f"Dibujando línea de {self.p1} a {self.p2}"
+
+    def _str_(self):
+        return f"Linea({self.p1}, {self.p2})"
+
+class Circulo:
+    def _init_(self, centro: Punto, radio: float):
+        self.centro = centro
+        self.radio = radio
+
+    def dibujaCirculo(self):
+        return f"Dibujando círculo con centro en {self.centro} y radio {self.radio}"
+
+    def _str_(self):
+        return f"Circulo(Centro: {self.centro}, Radio: {self.radio})"
+
+# Ejemplo de uso
+p1 = Punto(1, 2)
+p2 = Punto(4, 5)
+linea = Linea(p1, p2)
+circulo = Circulo(p1, 3)
+
+print(linea)  # Salida: Linea(Punto(1, 2), Punto(4, 5))
+print(linea.dibujaLinea())  # Salida: Dibujando línea de Punto(1, 2) a Punto(4, 5)
+
+print(circulo)  # Salida: Circulo(Centro: Punto(1, 2), Radio: 3)
+print(circulo.dibujaCirculo())  # Salida: Dibujando círculo con centro en Punto(1, 2) y radio 3
